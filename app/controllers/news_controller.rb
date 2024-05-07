0e3a73c143e29be496160fa401cecc1ad69a09c5
Allen Stevens <allen.stevens@students.dominican.edu>
2024-05-06 19:23:04 -0700

    added news model

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,10 +2,26 @@ require 'net/http'
 require 'uri'
 
 class NewsController < ApplicationController
-  def index
-    topic = params[:topic]
-    uri = URI("https://newsapi.org/v2/everything?q=#{topic}&apiKey=#{ENV['NEWS_API_KEY']}")
-    response = Net::HTTP.get(uri)
-    @news = JSON.parse(response)
+    def index
+        topic = params[:topic]
+        uri = URI("https://newsapi.org/v2/everything?q=#{topic}&apiKey=#{ENV['NEWS_API_KEY']}")
+        response = Net::HTTP.get(uri)
+        news_stories = JSON.parse(response)['articles']
+        
+        news_stories.each do |story|
+            News.create(title: story['title'], author: story['author'], content: story['content'])
+        end
+        
+        @news = News.all
+        end
+
+  def create
   end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get '/news', to: 'news#index'
+
+  resources :news, only: [:create, :update, :destroy]
 end
