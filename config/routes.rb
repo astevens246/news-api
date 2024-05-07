b8ab3d3b107f11806b84995d764ed8241fa337e6
Allen Stevens <allen.stevens@students.dominican.edu>
2024-05-06 20:41:54 -0700

    added views for news stories

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   get '/news', to: 'news#index'
+  get '/news/fetch', to: 'news#fetch'
+  root 'news#fetch'
 
   resources :news, only: [:create, :update, :destroy]
 end
